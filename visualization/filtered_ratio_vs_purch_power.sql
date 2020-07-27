684daab25b41457e49069d1ad29efb1b2dfd3714
R Sanjabi <rebecca.sanjabi@gmail.com>
2020-07-26 17:39:35 -0700

    initial queries

---
+++ visualization/filtered_ratio_vs_purch_power.sql
@@ -0,0 +1,74 @@
+------------------------------------------
+-- Filtered to All vs Purchasing Power
+------------------------------------------
+
+WITH recent_listings AS (
+
+    -- Self join for only latest listing info gathered in the last year
+    SELECT
+        l1.listing_snapshot_key,
+        l1.standard_city,
+        l1.listing_id
+    FROM fct_listing_snapshot AS l1
+    LEFT OUTER JOIN fct_listing_snapshot AS l2
+        ON (l1.listing_id = l2.listing_id
+            AND l1.listing_report_date > l2.listing_report_date)
+    WHERE l2.listing_id IS NULL
+        AND l1.listing_report_date > dateadd(month, -12, getdate())
+
+),
+
+all_listings_count AS (
+
+    SELECT
+        count(*) AS all_list_count,
+        dim_listing.standard_city
+    FROM dim_listing
+    INNER JOIN recent_listings
+        ON recent_listings.listing_id = dim_listing.listing_id
+    GROUP BY 2
+
+),
+
+filtered_listings AS (
+
+    SELECT listing_snapshot_key
+    FROM analytics_flagged
+    WHERE 1 = 1
+        [[AND {{FREQ_BOOK_FLAG}}]]
+        [[AND {{HOME_FLAG}}]]
+        [[AND {{HIGH_AVAIL_FLAG}}]]
+        [[AND {{MULTI_HOST_FLAG}}]]
+
+),
+
+filtered_listings_count AS (
+    SELECT
+        count(DISTINCT(recent.listing_id)) AS flagged_list_count,
+        recent.standard_city
+    FROM filtered_listings
+    INNER JOIN recent_listings      AS recent
+        ON filtered_listings.listing_snapshot_key = recent.listing_snapshot_key
+    GROUP BY 2
+
+),
+
+cities_with_counts AS (
+
+    SELECT
+        filtered.standard_city,
+        dim_city.purch_power_idx,
+        filtered.flagged_list_count,
+        all_list.all_list_count,
+        round(filtered.flagged_list_count
+                / all_list.all_list_count, 2) AS flagged_to_all_ratio
+    FROM filtered_listings_count    AS filtered
+    LEFT JOIN all_listings_count    AS all_list
+        ON filtered.standard_city = all_list.standard_city
+    LEFT JOIN dim_city
+        ON filtered.standard_city = dim_city.standard_city
+    ORDER BY 5
+
+)
+
+SELECT * FROM cities_with_counts
--- visualization/listings_per_100k.sql
+++ visualization/listings_per_100k.sql
@@ -1,9 +1,12 @@
--- Total Listings
+-----------------------------------------
+-- Top 10 cities by 100k people listings
+-----------------------------------------
 
 WITH most_recent_listings AS (
 
     -- Self join for only latest listing info gathered in the last year
     SELECT
+        l1.listing_id,
         l1.listing_snapshot_key,
         l1.standard_city
     FROM fct_listing_snapshot AS l1
@@ -17,10 +20,12 @@ WITH most_recent_listings AS (
 
 filtered_listings AS (
 
+    -- Filter out selected flags
     SELECT
+        recent.listing_id,
         recent.standard_city
-    FROM most_recent_listings   AS recent
-    JOIN analytics_flagged      AS flag
+    FROM most_recent_listings       AS recent
+    INNER JOIN analytics_flagged    AS flag
         ON recent.listing_snapshot_key = flag.listing_snapshot_key
     WHERE 1 = 1
         [[AND {{FREQ_BOOK_FLAG}}]]
@@ -30,15 +35,19 @@ filtered_listings AS (
 
 ),
 
-listings_per_city AS (
+listings_per100k_percity AS (
 
     SELECT
-        standard_city,
-        count(*)                AS total_listings_per_city
+        dim_city.standard_city,
+        dim_city.population,
+        round(count(filtered_listings.listing_id)
+                    / (dim_city.population / 100000), 0) AS listings_per_100k
     FROM filtered_listings
-    GROUP BY 1
-    ORDER BY 2 DESC
+    INNER JOIN dim_city
+        ON filtered_listings.standard_city = dim_city.standard_city
+    GROUP BY 1, 2
+    ORDER BY 3 DESC
     LIMIT 10
 )
 
-SELECT * FROM listings_per_city
+SELECT * FROM listings_per100k_percity
